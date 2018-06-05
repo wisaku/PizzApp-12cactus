feb1841266d462597c0554ebcf47b7b7277a7985
Emanuel <salgadoema@gmail.com>
2018-06-05 07:48:38 -0300

    agregue dos test de productos

--- PizzApp/src/main/java/modelo/Producto.java
+++ PizzApp/src/main/java/modelo/Producto.java
@@ -8,5 +8,27 @@ public class Producto extends Entity{
     private String nombre;
     private double precio;
 
-    public Producto(){}
+    public Producto()
+    {
+      
+    }
+
+
+    public void setNombre(String unNombre)
+    {
+        this.nombre=unNombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setPrecio(double unPrecio)
+    {
+        this.precio=unPrecio;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
 }
--- PizzApp/src/test/java/ProductoTestCase.java
+++ PizzApp/src/test/java/ProductoTestCase.java
@@ -6,6 +6,27 @@ import static org.junit.Assert.*;
 public class ProductoTestCase {
 
     @Test
+    public void creoUnProductoPizzaMozzarellaYCuandoLePreguntoSuNombreDicePizzaMozzarella(){
+
+        Producto producto = new Producto();
+        producto.setNombre("Pizza Mozzarella");
+
+        assertEquals("Pizza Mozzarella", producto.getNombre());
+
+    }
+
+    @Test
+    public void creoUnProductoPizzaMozzarellaDePrecio100YCuandoLePreguntoSuPrecioMeDice100(){
+
+        Producto producto = new Producto();
+        producto.setNombre("Pizza Mozzarella");
+        producto.setPrecio(100);
+        assertEquals(100, producto.getPrecio(),0);
+    }
+
+
+
+    @Test
     public void comoAdminQuieroDarDeAltaProductosParaAgregarALosPedidos(){
 
         Admin admin = new Admin();
