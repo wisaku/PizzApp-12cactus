6db48a9ff9fb53910a613434d5a0015c2b7d8b1d
Sir Demian <sirdemian@gmail.com>
2018-06-10 18:13:07 -0300

    actualizando desde upstream merge con mi frontEnd

--- PizzApp-Front/src/app/app.module.ts
+++ PizzApp-Front/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { APP_ROUTING} from './app.routes';
 // Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
 import { NuevoPedidoModule } from './components/nuevo-pedido/nuevo-pedido.module';
 import { CrearClienteComponent } from './components/crear-cliente/crear-cliente.component';
 import { HomeClientesComponent } from './components/home-clientes/home-clientes.component';
