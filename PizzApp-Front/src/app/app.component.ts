815ef5b6079a95c73878ae904add9ff996a0e1a5
Sir Demian <sirdemian@gmail.com>
2018-06-11 01:02:48 -0300

    se agregaron botones sin funcionalidad a la vista

--- PizzApp-Front/src/app/app.component.ts
+++ PizzApp-Front/src/app/app.component.ts
@@ -23,36 +23,6 @@ export class AppComponent {
         console.log('problemas');
       })
    }
-
-
- 
-
-/*  @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
-  })
-  /*export class AppComponent {
-    nombre = 'Rodriguez Pablo';
-    edad = 40;
-    email = 'rpablo@gmail.com';
-    sueldos = [1700, 1600, 1900];
-    activo = true;
-  
-    esActivo() {
-      if (this.activo)
-        return 'Trabajador Activo';
-      else
-        return 'Trabajador Inactivo';
-    }
-  
-    ultimos3Sueldos() {
-      let suma=0;
-      for(let x=0; x<this.sueldos.length; x++)
-        suma+=this.sueldos[x];
-      return suma;
-    }*/
-
     
 }  
 
