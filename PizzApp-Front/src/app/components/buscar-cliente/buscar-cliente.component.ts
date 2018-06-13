d54d43431e2165cb88965789bbf73c165b4ee839
Sir Demian <sirdemian@gmail.com>
2018-06-13 12:09:58 -0300

    Se agregó funcionalidad mostrar clientes en la vista

--- PizzApp-Front/src/app/components/buscar-cliente/buscar-cliente.component.ts
+++ PizzApp-Front/src/app/components/buscar-cliente/buscar-cliente.component.ts
@@ -21,6 +21,7 @@ export class BuscarClienteComponent implements OnInit {
     this.http.get("http://localhost:8080/PizzApp/rest/clienteService/todosLosClientes")
     .subscribe(
       result => {
+        console.log(result);
         this.clientes = result;
       },
       error => {
--- PizzApp-Front/src/app/nuevo-producto/nuevo-producto.component.ts
+++ PizzApp-Front/src/app/nuevo-producto/nuevo-producto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-nuevo-producto',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuevo-producto.component.css']
 })
 export class NuevoProductoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  clientes = null;
+  
+  constructor(private http: HttpClient) { }
+    
+    ngOnInit() {
+    
+   }
+  
 
 }
